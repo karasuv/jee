2bc3fd2e20c4bdabec3a407b8de56a169a62fb48
karasuv <karasuv@inbox.ru>
2021-11-29 19:16:11 +0300

    сразу все работало, а потом пишет Ошибка создания экземпляра класса сервлета [ru.gb.FirstServlet]

---
+++ src/main/java/ru/gb/FirstServlet.java
@@ -0,0 +1,20 @@
+package ru.gb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().printf("hello world spring");
+
+    }
+}
